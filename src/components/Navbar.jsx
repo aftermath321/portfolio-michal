b90c037e8099fd5f076f3478fcdce6b0e8a298ef
Michał Kiełbasa <michal.kielbasa1994@gmail.com>
2022-10-31 18:00:14 +0100

    Intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { FaTimes } from 'react-icons/fa'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { SiGmail } from 'react-icons/si'
+import { MdContactPage } from 'react-icons/md'
+
+
+
+const Navbar = () => {
+    const [mobile, setMobile] = useState(false);
+
+    const handleClick = () => setMobile(!mobile)
+    
+    return (
+        <div className='fixed w-full flex h-[80px] justify-between items-center px-4'>
+                <div>
+
+                </div>
+
+            {/* Top right menu */}
+            <div>
+                <nav>
+                    <ul className='hidden md:flex'>
+                        <li className='text-xl font-bold'>Home</li>
+                        <li className='text-xl font-bold'>About</li>
+                        <li className='text-xl font-bold'>Skills</li>
+                        <li className='text-xl font-bold'>Experience</li>
+                        <li className='text-xl font-bold'>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+
+            {/* Mobile menu icon */}
+            <div className='z-10 md:hidden text-2xl py-4 absolute right-10' onClick={handleClick}>
+                {!mobile ? <GiHamburgerMenu /> : <FaTimes />}
+            </div>
+            
+
+            {/* Mobile menu */}
+            <div className={!mobile ? 'hidden' : 'absolute justify-center items-center flex flex-col top-0 left-0 h-screen w-full'}>
+                <ul >
+                    <li onClick={handleClick} className='py-4 text-2xl font-bold'>Home</li>
+                    <li onClick={handleClick} className='py-4 text-2xl font-bold'>About</li>
+                    <li onClick={handleClick} className='py-4 text-2xl font-bold'>Skills</li>
+                    <li onClick={handleClick} className='py-4 text-2xl font-bold'>Experience</li>
+                    <li onClick={handleClick} className='py-4 text-2xl font-bold'>Contact</li>
+                </ul>
+            </div>
+
+            {/* Side menu */}
+            <div className='hidden fixed lg:flex left-0 top-[35%]'>
+                <ul>
+                    <li className='w-[160px] h-[50px] bg-white items-center justify-center flex ml-[-100px] duration-300 hover:ml-[-10px]'>
+                        <a href='/' className='flex justify-between items-center w-full '> Github <BsGithub size={30} /> </a></li>
+                    <li className='w-[160px] h-[50px] bg-white items-center justify-center flex ml-[-100px] duration-300 hover:ml-[-10px]'><a href='/' className='flex justify-between items-center w-full '> Email <SiGmail size={30} /> </a></li>
+                    <li className='w-[160px] h-[50px] bg-white items-center justify-center flex ml-[-100px] duration-300 hover:ml-[-10px]'><a href='/' className='flex justify-between items-center w-full '>  LinkedIn <BsLinkedin size={30} /> </a></li>
+                    <li className='w-[160px] h-[50px] bg-white items-center justify-center flex ml-[-100px] duration-300 hover:ml-[-10px]'><a href='/' className='flex justify-between items-center w-full '> Resume <MdContactPage size={30} /> </a></li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
